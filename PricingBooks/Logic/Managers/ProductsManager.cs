670a5a2993c741d4987669548bd03b62713c0e77
Boris Pedraza <bpedrazaa@gmail.com>
2021-05-08 21:41:26 -0400

    PB-06 Calculation for the promotion price created in the ProductsManager.cs

--- PricingBooks/Logic/Managers/ProductsManager.cs
+++ PricingBooks/Logic/Managers/ProductsManager.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Text;
 using UPB.PricingBooks.Data;
 using UPB.PricingBooks.Logic.Models;
+using UPB.PricingBooks.Services;
 
 namespace UPB.PricingBooks.Logic.Managers
 {
     public class ProductsManager : IProductsManager
     {
         private readonly IDbContext _dbContext;
+        private readonly ICampaignService _campaignService;
 
-        public ProductsManager(IDbContext dbContext)
+        public ProductsManager(IDbContext dbContext, ICampaignService campaignService)
         {
             _dbContext = dbContext;
+            _campaignService = campaignService;
         }
 
         public List<Product> GetProducts()
@@ -22,6 +25,20 @@ namespace UPB.PricingBooks.Logic.Managers
 
         public Product CreateProduct(Product product)
         {
+            // Control for promotion price based on Campaign microservice
+            string codeCampaign = _campaignService.GetCampaign().Result.Code;
+
+            if (codeCampaign == "XMAS" || codeCampaign == "xmas") {
+                product.PromotionPrice = product.FixedPrice * 0.05;
+            }else if(codeCampaign == "SUMMER" || codeCampaign == "summer")
+            {
+                product.PromotionPrice = product.FixedPrice * 0.2;
+            }
+            else if(codeCampaign == "BFRIDAY" || codeCampaign == "bfriday")
+            {
+                product.PromotionPrice = product.FixedPrice * 0.25;
+            }
+
             _dbContext.AddProduct(DTOMappers.MapProductLD(product));
             return product;
         }
