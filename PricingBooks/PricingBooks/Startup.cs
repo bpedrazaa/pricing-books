cfdaf4740f787d9ba2739da7c6b0a6094e51339d
Boris Pedraza <bpedrazaa@gmail.com>
2021-05-09 12:47:51 -0400

    PB-09 Logs and exceptions for ProductsManager.cs file created, appsettings.json file modified to specify LogPath

--- PricingBooks/PricingBooks/Startup.cs
+++ PricingBooks/PricingBooks/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,6 +31,14 @@ namespace PricingBooks
                 .AddEnvironmentVariables();
 
             Configuration = builder.Build();
+
+            //Serilog Configuration
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel
+                .Information()
+                .WriteTo.File(Configuration.GetSection("Project").GetSection("LogPath").Value)
+                .CreateLogger();
+            Log.Information($"In environment: {env.EnvironmentName}");
         }
 
 
